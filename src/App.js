bc0b3a3a9ff090d6f493428cf581e0bdebbbce92
PinkPaca <fmadkdl113@gmail.com>
2023-04-26 21:16:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,28 +3,44 @@ import {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';   //logo spin
 
-class App extends Component() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      monsters:[
+      {
+        name: 'Linda',
+        id: '123eae123'
+      },
+      {
+        name: 'Frank',
+        id: '123ea23'
+      },
+      {
+        name: 'Jacky',
+        id: '1ae123'
+      },
+      {
+        name: 'Andrei',
+        id: '1asd3'
+      }
+    ]
+    };
+  }
+
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-             <code>Hello</code>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map((monster)=>{  //iterate
+            return <div key={monster.id}>
+            <h1>{monster.name}</h1>
+            </div>;
+          })
+        }
       </div>
     );
   }
-  
 }
 
 export default App;
