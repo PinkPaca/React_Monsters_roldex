53ac92224f56994252c515ce4d8987f26e90b39d
PinkPaca <fmadkdl113@gmail.com>
2023-05-03 21:57:02 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ class App extends Component {
 
     this.state={
       monsters: [],
+      searchField:''
     };
+    console.log('constructor');
   }
 
   componentDidMount(){
+    console.log('componentDidMound');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => 
     response.json())//promise
@@ -25,11 +28,34 @@ class App extends Component {
     ));
   }
 
+
+  onSearchChange=(event)=>{       //optimization
+    const searchField=event.target.value.toLocaleLowerCase();
+
+    this.setState(()=>{
+      return {searchField};
+    })
+  }
+
   render(){
+    console.log('render');
+
+    const { monsters, searchField } =this.state;    //optimization
+    const { onSearchChange } = this;
+
+    const filteredMonster=monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField); //in the render out of
+    });
+
     return (
       <div className="App">
+      <input 
+        className='search-box' 
+        type='search' 
+        placeholder='search monsters' 
+        onChange={onSearchChange}/>
         {
-          this.state.monsters.map((monster)=>{  //iterate
+          filteredMonster.map((monster)=>{  //iterate
             return <div key={monster.id}>
             <h1>{monster.name}</h1>
             </div>;
